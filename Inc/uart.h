87a3f7671f6ac0c71cb3aaaf9150eabac2c4cfe0
joseph lee Canton <josephleecanton@gmail.com>
2023-06-02 18:36:45 -0400

    uart init now in uart.h file

--- Inc/uart.h
+++ Inc/uart.h
@@ -12,6 +12,6 @@
 
 #endif /* UART_H_ */
 
-static uint16_t compute_baud_rate(uint32_t periph_clk,uint32_t baudrate);
-static void set_baudrate(uint32_t periph_clk,uint32_t baudrate);
-static void uart_write(int ch);
+
+void uart_write(int ch);
+void uart_init();
